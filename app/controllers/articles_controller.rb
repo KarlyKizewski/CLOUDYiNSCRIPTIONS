5ade62d45e6019ce3d3c28a960a4273b5e002ba7
Karly Kizewski <kizewski.karly@gmail.com>
2019-08-18 18:17:42 +0000

    css adjustments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,6 +45,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:manuscript, :title, :text)
   end
 end
