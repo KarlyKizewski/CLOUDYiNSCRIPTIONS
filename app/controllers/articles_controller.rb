7090fe3f3679e9c56cd31ac567c351e301d0c626
Karly Kizewski <kizewski.karly@gmail.com>
2019-08-18 23:04:31 +0000

    removed basic http auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"], except: [:index, :show]
 
   def index
     @articles = Article.paginate(:page => params[:page], :per_page => 8)
